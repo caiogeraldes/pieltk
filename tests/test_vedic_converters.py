c2ffd9bdad2421c1ff3a6692fb9084196dec5766
Caio Geraldes <caioaguida@protonmail.com>
2021-09-14 20:30:32 -0300

    update tests

--- tests/test_vedic_converters.py
+++ tests/test_vedic_converters.py
@@ -1,8 +1,8 @@
-from pieltk.converters.vedic import AsciiConverter
+from pieltk.converters.vedic import AsciiDevanagari
 
 
 def test_deva() -> None:
-    ascii_replace = AsciiConverter(scheme="hk_to_deva", udatta_to_anudatta=True)
+    ascii_replace = AsciiDevanagari(scheme="hk_to_deva", udatta_to_anudatta=True)
     deva = "अ॒ग्निमी॑ळे पु॒रोहि॑तं य॒ज्ञस्य॑ दे॒वमृ॒त्विज॑म्"
     hk = "agni/mILe puro/hitaM yajJa/sya deva/mRtvi/jam"
     assert ascii_replace.converter(hk) == deva
